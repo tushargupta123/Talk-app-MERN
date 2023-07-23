85b47e8ab72a82cdf481b1ac1d0315c16f072c27
tushargupta123 <tushargupta2k3@gmail.com>
2023-07-23 22:06:30 +0530

    updated cors again

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ app.use(cors(
         credentials: true
     }
 ))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://lets-chat-beta.vercel.app');
+    next();
+  });
 app.use(express.json());
 app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes);

0289d5696ae4a8a0eff4476a80ed8d80c96c6f37
tushargupta123 <tushargupta2k3@gmail.com>
2023-07-24 19:45:12 +0530

    minor

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const connect = require("./config/db");
 const app = express();
-const cors = require("cors");
-app.use(cors());
-
+// const cors = require("cors");
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");

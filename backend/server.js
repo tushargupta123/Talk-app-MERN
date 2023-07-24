29aec19be2e8030cb9710b9df431bf492599cf8e
tushargupta123 <tushargupta2k3@gmail.com>
2023-07-24 19:49:45 +0530

    g5

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const connect = require("./config/db");
 const app = express();
-// const cors = require("cors");
-// app.use(cors());
-app.use((req, res, next) => {
-  req.header("Access-Control-Allow-Origin", "*");
-  req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const cors = require("cors");
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
@@ -19,46 +19,46 @@ app.use("/chat", chatRoutes);
 app.use("/message", messageRoutes);
 
 const server = http.createServer(app); // Create a server using http module
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "https://lets-chat-two-khaki.vercel.app",
-  },
-  pingTimeout: 60000,
-});
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "https://lets-chat-two-khaki.vercel.app",
+//   },
+//   pingTimeout: 60000,
+// });
 
-io.on("connection", (socket) => {
-  console.log("connected to server.io");
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
-  });
+// io.on("connection", (socket) => {
+//   console.log("connected to server.io");
+//   socket.on("setup", (userData) => {
+//     socket.join(userData._id);
+//     socket.emit("connected");
+//   });
 
-  socket.on("join chat", (room) => {
-    socket.join(room);
-  });
+//   socket.on("join chat", (room) => {
+//     socket.join(room);
+//   });
 
-  socket.on("typing", (room) => {
-    socket.in(room).emit("typing");
-  });
-  socket.on("stop typing", (room) => {
-    socket.in(room).emit("stop typing");
-  });
+//   socket.on("typing", (room) => {
+//     socket.in(room).emit("typing");
+//   });
+//   socket.on("stop typing", (room) => {
+//     socket.in(room).emit("stop typing");
+//   });
 
-  socket.on("new message", (newMessageReceived) => {
-    var chat = newMessageReceived.chat;
-    if (!chat.users) {
-      return;
-    }
-    chat.users.forEach((user) => {
-      if (user._id === newMessageReceived.sender._id) return;
-      socket.in(user._id).emit("message received", newMessageReceived);
-    });
-  });
+//   socket.on("new message", (newMessageReceived) => {
+//     var chat = newMessageReceived.chat;
+//     if (!chat.users) {
+//       return;
+//     }
+//     chat.users.forEach((user) => {
+//       if (user._id === newMessageReceived.sender._id) return;
+//       socket.in(user._id).emit("message received", newMessageReceived);
+//     });
+//   });
 
-  socket.off("disconnect", () => {
-    console.log("disconnected from server.io");
-  });
-});
+//   socket.off("disconnect", () => {
+//     console.log("disconnected from server.io");
+//   });
+// });
 
 const PORT = 5000;
 server.listen(PORT, () => {

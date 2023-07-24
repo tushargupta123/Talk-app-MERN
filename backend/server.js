27a3336db1c5c8ad448de9c37b7a45c03986517b
tushargupta123 <tushargupta2k3@gmail.com>
2023-07-24 19:47:57 +0530

    fbgf

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const app = express();
 // const cors = require("cors");
 // app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  req.header("Access-Control-Allow-Origin", "*");
+  req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 const userRoutes = require("./routes/userRoutes");

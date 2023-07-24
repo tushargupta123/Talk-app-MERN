462594b0fcfaa1e24b3b109f9fff9923263a78f1
tushargupta123 <tushargupta2k3@gmail.com>
2023-07-24 20:05:02 +0530

    5t5

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,7 @@ const connect = require("./config/db");
 const app = express();
 const cors = require("cors");
 app.use("/", require("./proxy"));
-app.use(cors({
-  origin:true
-}));
+app.use(cors());
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
